b765293f0713ae432c14d515289c73d793ddf4dd
ShagDasha10 <162694007+Dashaht@users.noreply.github.com>
2025-07-21 16:47:44 +0300

    Add files via upload

--- www/config.php
+++ www/config.php
@@ -5,6 +5,6 @@ define('BOT_LOGS_DIRECTORY', BOT_BASE_DIRECTORY.'/logs');
 define('BOT_IMAGES_DIRECTORY', BOT_BASE_DIRECTORY.'/static');
 define('BOT_AUDIO_DIRECTORY', BOT_BASE_DIRECTORY.'/audio');
 
-define('CALLBACK_API_CONFIRMATION_TOKEN', 'bb112c41'); //Строка для подтверждения адреса сервера из настроек Callback API
-define('VK_API_ACCESS_TOKEN', '016cb55230eb83e34f620389s149345d975bc12e2fb6f30e0a833a51d345d0d123c3dc0abc1c864036sdf989fb8345'); //Ключ доступа сообщества
-define('YANDEX_API_KEY', '30e3213440-61233-1294-b3415-471212369886'); //Ключ для доступа к Yandex Speech Kit
+define('CALLBACK_API_CONFIRMATION_TOKEN', '7f395fd6'); //Строка для подтверждения адреса сервера из настроек Callback API
+define('VK_API_ACCESS_TOKEN', 'vk1.a.5HM6-YZq6_urw8jYITZ30Mmw2vkSlvyAIWV3rcjv68Wt00p4fCv9K48yJbduIjnVeSWAE5e3ub8RhpLV9gos2Qsoee0N1ytqera-lbFKve3n9G0lsiU6jgEPliwVFcb5WiaYjOrBjQjj53kJXSz1Qb_dSrXzVNm7j291ew34g2VTRajulvDY5lS0iqQtKMTou459_mvnhhyMIRdjgUJQxQ'); //Ключ доступа сообщества
+define('YANDEX_API_KEY', ' '); //Ключ для доступа к Yandex Speech Kit
